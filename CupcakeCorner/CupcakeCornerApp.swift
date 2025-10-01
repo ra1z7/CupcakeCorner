ed20b151ec073d75102cef25a059b2f489779b86
ra1z <purnamaanrai7@gmail.com>
2025-10-01 09:51:43 +0545

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Purnaman Rai (College) on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
