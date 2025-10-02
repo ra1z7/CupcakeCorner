b837c1cc522d5cbbf3803283122f564d30491e69
ra1z <purnamaanrai7@gmail.com>
2025-10-02 10:47:04 +0545

    Display no. of results and increase its count

--- CupcakeCorner/FindSongs.swift
+++ CupcakeCorner/FindSongs.swift
@@ -16,6 +16,7 @@ struct FindSongs: View {
     }
     
     struct Response: Codable {
+        let resultCount: Int
         let results: [Song]
     }
     
@@ -23,28 +24,33 @@ struct FindSongs: View {
     
     
     @State private var songs = [Song]()
+    @State private var songsCount = 0
     
     var body: some View {
         NavigationStack {
-            List(songs, id: \.trackId) { song in
-                HStack(spacing: 15) {
-                    AsyncImage(url: URL(string: song.artworkUrl100)) { songArtwork in
-                        songArtwork
-                            .resizable()
-                            .scaledToFill()
-                            .clipShape(.rect(cornerRadius: 15))
-                    } placeholder: {
-                        ProgressView()
+            List {
+                Section("Total Songs: \(songsCount)"){
+                    ForEach(songs, id: \.trackId) { song in
+                        HStack(spacing: 15) {
+                            AsyncImage(url: URL(string: song.artworkUrl100)) { songArtwork in
+                                songArtwork
+                                    .resizable()
+                                    .scaledToFill()
+                                    .clipShape(.rect(cornerRadius: 15))
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 50, height: 50)
+                            
+                            VStack(alignment: .leading) {
+                                Text(song.trackName)
+                                    .font(.headline)
+                                Text(song.collectionName)
+                                    .foregroundStyle(.secondary)
+                            }
+                            .lineLimit(1)
+                        }
                     }
-                    .frame(width: 50, height: 50)
-                    
-                    VStack(alignment: .leading) {
-                        Text(song.trackName)
-                            .font(.headline)
-                        Text(song.collectionName)
-                            .foregroundStyle(.secondary)
-                    }
-                    .lineLimit(1)
                 }
             }
             .navigationTitle("Sajjan Raj Vaidya")
@@ -55,7 +61,7 @@ struct FindSongs: View {
     }
     
     func loadSongs() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=sajjan+raj+vaidya&entity=song") else {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=sajjan+raj+vaidya&entity=song&limit=200") else {
             print("Invalid URL")
             return
         }
@@ -64,6 +70,7 @@ struct FindSongs: View {
             let (downloadedData, _) = try await URLSession.shared.data(from: url)
             let decodedData = try JSONDecoder().decode(Response.self, from: downloadedData)
             songs = decodedData.results
+            songsCount = decodedData.resultCount
         } catch {
             print("Unable to download data")
         }
